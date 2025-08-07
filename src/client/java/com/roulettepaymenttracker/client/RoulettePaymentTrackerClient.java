f764849b40d3eba5af8e1866e613549b90c98aaa
Szymon Lenard <szymon.lenard@icloud.com>
2025-08-07 17:05:00 +0200

    chore: creating a project

---
+++ src/client/java/com/roulettepaymenttracker/client/RoulettePaymentTrackerClient.java
@@ -0,0 +1,10 @@
+package com.roulettepaymenttracker.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RoulettePaymentTrackerClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/com/roulettepaymenttracker/RoulettePaymentTracker.java
@@ -0,0 +1,10 @@
+package com.roulettepaymenttracker;
+
+import net.fabricmc.api.ModInitializer;
+
+public class RoulettePaymentTracker implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
