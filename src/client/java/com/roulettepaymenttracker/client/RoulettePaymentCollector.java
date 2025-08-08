f4a18a77e8901236d4208ab77905909d1709e08f
Szymon Lenard <szymon.lenard@icloud.com>
2025-08-08 13:24:53 +0200

    refactor: optimized roulettepaymentcollector

---
+++ src/client/java/com/roulettepaymenttracker/client/RoulettePaymentCollector.java
@@ -0,0 +1,68 @@
+package com.roulettepaymenttracker.client;
+
+import net.fabricmc.fabric.api.client.message.v1.ClientReceiveMessageEvents;
+import net.minecraft.text.Text;
+import net.minecraft.text.TextContent;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.lang.reflect.Method;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class RoulettePaymentCollector {
+
+    private static final String paymentFirstComponent = "Otrzymałeś:"; // the word that will be checked with payment message
+    private static final int positionFirstWord = 1; // where's located first word that player want to use for checking
+    private static final int positionAmount = 2; // where's located amount that player has sent
+    private static final int positionUsername = 4; // where's located player's username
+    private static final int paymentComponentsSize = 5; // what's the size of the payment message
+    public String paymentAmount; // amount that user sent in payment
+    public String paymentUser; // name of the user that sent the payment
+    public void getMessage() {
+        ClientReceiveMessageEvents.GAME.register((text, overlay) -> {
+            List<Text> paymentComponents = new ArrayList<>();
+            collectAllTextComponents(text, paymentComponents); // collects all components
+
+            try {
+                    TextContent firstContentComponent = paymentComponents.get(positionFirstWord).getContent(); // get the part of the message that starts with specified later word
+                    String firstContentString = extractStringFromComponent(firstContentComponent);
+                    String firstContentStringSpaceless = StringUtils.deleteWhitespace(firstContentString);
+
+                    // checks if the first word specified by player and first word, that have position specified by player, the same
+                    boolean areFirstWordsEqual = firstContentStringSpaceless.equals(paymentFirstComponent);
+
+                    if(areFirstWordsEqual && paymentComponents.size() == paymentComponentsSize) {
+                        try {
+                            TextContent priceContentComponent = paymentComponents.get(positionAmount).getContent(); // get the part of the message that starts with payment amount
+                            this.paymentAmount = extractStringFromComponent(priceContentComponent);
+                            System.out.println("Payment Amount: " + this.paymentAmount);
+
+                            this.paymentUser = paymentComponents.get(positionUsername).getString(); // gets payment username
+                            System.out.println("Payment User: " + this.paymentUser);
+                        } catch (Exception exception) {
+                            System.out.println("Failed to retrieve payment price and username: " + exception.getMessage());
+                        }
+                    }
+            } catch (Exception exception) {
+                System.out.println("Payment message pre-check failed: " + exception.getMessage());
+            }
+        });
+    }
+
+    // changes specified component from payment message to string
+    private String extractStringFromComponent(TextContent component) throws Exception {
+        Method priceMethod = component.getClass().getDeclaredMethod("string"); // gets the method .string() from the calss that is not showed
+        priceMethod.setAccessible(true); // bypasses Java access checks444
+
+        return (String) priceMethod.invoke(component); // calls method on the object wiht no arguments
+    }
+
+    // collects all components from payment message and turns them into a List<Text>
+    private void collectAllTextComponents(Text root, List<Text> collector) {
+        for (Text sibling : root.getSiblings()) {
+            collector.add(sibling);
+            collectAllTextComponents(sibling, collector);
+        }
+    }
+}
--- src/client/java/com/roulettepaymenttracker/client/RoulettePaymentTrackerClient.java
+++ src/client/java/com/roulettepaymenttracker/client/RoulettePaymentTrackerClient.java
@@ -1,64 +1,13 @@
 package com.roulettepaymenttracker.client;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.message.v1.ClientReceiveMessageEvents;
-import net.minecraft.text.Text;
-import net.minecraft.text.TextContent;
-
-import java.awt.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.lang.reflect.Method;
-
-import org.apache.commons.lang3.StringUtils;
 
 public class RoulettePaymentTrackerClient implements ClientModInitializer {
 
+    RoulettePaymentCollector collector = new RoulettePaymentCollector();
+
     @Override
     public void onInitializeClient() {
-        getMessage();
-    }
-
-    static String paymentFirstComponent = "Otrzymałeś:";
-    String paymentAmount;
-    String paymentUser;
-    public void getMessage() {
-        ClientReceiveMessageEvents.GAME.register((text, overlay) -> {
-            List<Text> paymentComponents = new ArrayList<>();
-            collectAllTextComponents(text, paymentComponents); // collects all components
-
-            try {
-                    TextContent firstContentComponent = paymentComponents.get(1).getContent(); // get the part of the message that starts with payment amount
-                    Method firstContentMethod = firstContentComponent.getClass().getDeclaredMethod("string"); // gets the method .string() from the calss that is not showed
-                    firstContentMethod.setAccessible(true); // bypasses Java access checks
-
-                    String firstContentString = (String) firstContentMethod.invoke(firstContentComponent); // calls method on the object wiht no arguments
-
-                    if(StringUtils.deleteWhitespace(firstContentString).equals(StringUtils.deleteWhitespace(paymentFirstComponent)) && paymentComponents.size() == 5) {
-                        try {
-                            TextContent priceContentComponent = paymentComponents.get(2).getContent(); // get the part of the message that starts with payment amount
-                            Method priceMethod = priceContentComponent.getClass().getDeclaredMethod("string"); // gets the method .string() from the calss that is not showed
-                            priceMethod.setAccessible(true); // bypasses Java access checks
-
-                            this.paymentAmount = (String) priceMethod.invoke(priceContentComponent); // calls method on the object wiht no arguments
-                            System.out.println("Payment Amount: " + this.paymentAmount);
-
-                            this.paymentUser = paymentComponents.get(4).getString(); // gets payment username
-                            System.out.println("Payment User: " + this.paymentUser);
-                        } catch (Exception exception) {
-                            System.out.println("Failed to retrieve payment price and username: " + exception.getMessage());
-                        }
-                    }
-            } catch (Exception exception) {
-                System.out.println("Payment message pre-check failed: " + exception.getMessage());
-            }
-        });
-    }
-
-    private void collectAllTextComponents(Text root, List<Text> collector) {
-        for (Text sibling : root.getSiblings()) {
-            collector.add(sibling);
-            collectAllTextComponents(sibling, collector);
-        }
+        collector.getMessage();
     }
 }
