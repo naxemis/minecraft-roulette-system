ba5cf35709016f4e7ebd3119b334b6be2d64c06a
naxemis <szymon.lenard@icloud.com>
2026-06-02 08:40:29 +0200

    chore(project): initialize default roulette mod and app projects

---
+++ RoulettePaymentHandler/src/client/java/dev/naxemis/roulettepaymenthandler/client/RoulettePaymentHandlerClient.java
@@ -0,0 +1,10 @@
+package dev.naxemis.roulettepaymenthandler.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RoulettePaymentHandlerClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ RoulettePaymentHandler/src/main/java/dev/naxemis/roulettepaymenthandler/RoulettePaymentHandler.java
@@ -0,0 +1,10 @@
+package dev.naxemis.roulettepaymenthandler;
+
+import net.fabricmc.api.ModInitializer;
+
+public class RoulettePaymentHandler implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
